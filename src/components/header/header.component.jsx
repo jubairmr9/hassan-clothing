8fd0bf25a201c685282a7d2b5fbfc943f993eeea
Jubair Hassan <jubairhassan@Jubairs-MacBook-Pro.local>
2022-01-07 09:57:05 -0600

    Implemented firebase utils. Added sign-up component. Included ability to store authenticated users in the firestore database

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { auth } from "../../firebase/firebase.utils";
 
-import { ReactComponent as Logo } from "../../assets/h-logo.svg"
+import { ReactComponent as Logo } from "../../assets/crown.svg"
 
 import "./header.style.scss"
 
