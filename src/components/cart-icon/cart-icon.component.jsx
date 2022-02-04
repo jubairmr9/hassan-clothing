ea48aae670a3344c3d7d1ca6dd072f3fe52471ea
Jubair Hassan <jubairhassan@Jubairs-MacBook-Pro.local>
2022-02-04 03:20:15 -0600

    Created the CartItem component and connected the CartDropdown using our cartItems reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -10,7 +10,7 @@ import "./cart-icon-styles.scss";
 const CartIcon = ({ toggleCartHidden }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <sapn className="item-count">0</sapn>
+        <span className="item-count">0</span>
     </div>
 );
 
