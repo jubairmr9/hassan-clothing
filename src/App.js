a61c8d42697b77df65c95dc985ca8e424a84f551
Jubair Hassan <jubairhassan@Jubairs-MacBook-Pro.local>
2022-02-05 22:26:53 -0600

    Added Checkout page, CheckoutItem component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import { createStructuredSelector } from "reselect";
 import "./App.css";
 
 import HomePage from './pages/homepage/homepage.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import Header from './components/header/header.component';
+
+import {
+  auth,
+  createUserProfileDocument
+} from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
 
@@ -45,6 +52,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
